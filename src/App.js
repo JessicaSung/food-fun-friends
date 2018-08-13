23a6e09e073450d0111ea6594fbefc8264d5c3c0
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2018-08-13 03:49:44 -0500

    display items and styled app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 import firebase from './firebase.js';
 
 class App extends Component {
   state = {
     currentItem: '',
-    username: ''
+    username: '',
+    items: []
   }
   handleChange = (e) => {
     // console.log(e, e.target.name, e.target.value);
@@ -29,22 +31,48 @@ class App extends Component {
       username: ''
     }));
   }
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        });
+      }
+      this.setState(() => ({
+        items: newState
+      }));
+    });
+  }
   render() {
     return (
       <div className='app'>
         <header>
           <h1>Fun Food Friends</h1>
         </header>
-        <div className='container'>
-          <section className='add-item'>
+        <div className="container">
+          <section className='add-item side'>
             <form onSubmit={this.handleSubmit}>
-              <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+              <input type="text" name="username" placeholder="Your name" onChange={this.handleChange} value={this.state.username} />
               <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
               <button>Add Item</button>
             </form>
           </section>
-          <section className='display-item'>
-            <ul></ul>
+          <section className='display-item side'>
+            <ul>
+              {this.state.items.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <h3>{item.title}</h3>
+                    <p>brought by: {item.user}</p>
+                  </li>
+                )
+              })}
+            </ul>
           </section>
         </div>
       </div>
